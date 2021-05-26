b080c4c973c09241511f0c904c01123dd0ac2f4b
Namart <anastasiya.triumf@gmail.com>
2021-05-26 22:57:49 +0300

    Updates

---
+++ precache-manifest.139e19159cfea3a0dae604dc30ab3082.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c1832c85694666d7305cbf091d94aa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "897e57c97f2ae12943ee",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "1a89982cc80606a56f83",
+    "url": "./static/css/main.c4e573a4.chunk.css"
+  },
+  {
+    "revision": "897e57c97f2ae12943ee",
+    "url": "./static/js/2.ea1416f0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ea1416f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a89982cc80606a56f83",
+    "url": "./static/js/main.d1abe8ce.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
